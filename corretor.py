9e8bccb4abeb75fa8e6565722afee66608cb37a1
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2022-08-31 15:19:00 -0300

    contador de palavras sem repeticoes

--- corretor.py
+++ corretor.py
@@ -1,14 +1,20 @@
 from nltk import word_tokenize
 from biblioteca import tokens_sem_pontuacao, normalizacao
 
+
 with open('dados/artigo_100_palavras.txt', 'r', encoding="utf8") as f: # f = file
-    artigo = f.read()
+    texto_fonte = f.read()
 
 
-tokens_artigo = word_tokenize(artigo) # Cria token para palavras e símbolos
+tokens_texto = word_tokenize(texto_fonte) # Cria token para palavras e símbolos
 
-tokens_alpha = tokens_sem_pontuacao(tokens_artigo)
+tokens_alpha = tokens_sem_pontuacao(tokens_texto)
 
 
 tokens_normalizados = normalizacao(tokens_alpha)
-print(tokens_normalizados)
+
+
+# Quantidade de palavras sem repetições
+tokens_sem_repeticao = set(tokens_normalizados)
+
+print(len(tokens_sem_repeticao))
